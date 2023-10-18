5455c430bc539a37d3367557bb956b0980fb8bd4
juyonLee00 <ljy012900@gmail.com>
2023-10-18 14:55:49 +0900

    [ADD] 플레이어-아이템 상호작용 추가중

---
+++ Assets/Scripts/Item/InteractionManager.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public interface IInteractable
+{
+    string GetInteractPrompt();
+    void OnInteract();
+}
+
+public class InteractionManager : MonoBehaviour
+{
+    public float checkRate = 0.05f;
+    private float lastCheckTime;
+    public float maxCheckDistance;
+    public LayerMask layerMask;
+
+    private GameObject curInteractGameobject;
+    private IInteractable curInteractable;
+
+    public TextMeshProUGUI promptText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - lastCheckTime > checkRate)
+        {
+            lastCheckTime = Time.time;
+
+            RaycastHit2D hit;
+
+            if (hit = Physics2D.Raycast(this.transform.position, this.transform.forward, maxCheckDistance, layerMask))
+            {
+                if (hit.collider.gameObject != curInteractGameobject)
+                {
+                    curInteractGameobject = hit.collider.gameObject;
+                    curInteractable = hit.collider.GetComponent<IInteractable>();
+                    Debug.Log(hit.collider.gameObject.name);
+                    //SetPromptText();
+                }
+            }
+            else
+            {
+                curInteractGameobject = null;
+                curInteractable = null;
+                promptText.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void SetPromptText()
+    {
+        promptText.gameObject.SetActive(true);
+        promptText.text = string.Format("<b>[E]</b> {0}", curInteractable.GetInteractPrompt());
+    }
+
+    public void OnInteractInput(InputAction.CallbackContext callbackContext)
+    {
+        if (callbackContext.phase == InputActionPhase.Started && curInteractable != null)
+        {
+            curInteractable.OnInteract();
+            curInteractGameobject = null;
+            curInteractable = null;
+            promptText.gameObject.SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/Item/ItemObject.cs
+++ Assets/Scripts/Item/ItemObject.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemObject : MonoBehaviour
+public class ItemObject : MonoBehaviour, IInteractable
 {
     public ItemData item;
 
