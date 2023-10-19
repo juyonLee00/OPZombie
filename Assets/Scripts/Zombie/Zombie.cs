c8f161cf47374537275c1c5aa785a165c56e1548
hibernate95 <trytostart95@gmail.com>
2023-10-19 19:53:01 +0900

    [ADD]AStarGrid floor층 그리드추가 시킴

--- Assets/Scripts/Zombie/Zombie.cs
+++ Assets/Scripts/Zombie/Zombie.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class ZombieScript : MonoBehaviour
 {
     public int health = 100; // 체력
-    public int attackDamage = 5; // 공격력
+    public int attackDamage = 1; // 공격력
     
 
     // 다른 스크립트와 상호 작용하거나 이벤트를 처리하는 메서드들을 추가할 수 있습니다.
@@ -24,4 +24,6 @@ public class ZombieScript : MonoBehaviour
         // 좀비가 사망했을 때 실행되는 로직을 추가할 수 있습니다.
         Destroy(gameObject);
     }
+
+
 }
