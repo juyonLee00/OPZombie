7c6b1da996f831d21b5901fde3bc67ad8dcd03b7
skybluesky-1005 <haneul9513@naver.com>
2023-10-19 22:51:07 +0900

    [FIX]아이템, 몹 스폰위치 오류 수정

--- Assets/Scripts/Map/RoomConfig.cs
+++ Assets/Scripts/Map/RoomConfig.cs
@@ -6,4 +6,5 @@ public class RoomConfig : ScriptableObject
     [Range(0, 100f)] public float monsterSpawnRate; // 몬스터가 스폰될 확률 (0~1)
     public GameObject[] zombies;
     public GameObject[] itemPrefabs; // 이 방에서 생성될 수 있는 아이템 프리팹들
+    public int itemSpawnCount;
 }
